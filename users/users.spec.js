ef645cd8427a86fd5af9c17b64df3b91f488cd90
damonbogich <damonbogich@gmail.com>
2020-03-05 18:52:14 -0500

    added get by id and delete tests to the users router

--- users/users.spec.js
+++ users/users.spec.js
@@ -24,4 +24,40 @@ describe('usersRouter', function() {
             })
         })
     })
+    describe('GET /:id', function() {
+        it('not allow user to login without token', function() {
+            //make a request to the api
+            return request(router)
+            // .set('Content-Type', 'application/json')
+            .get('/api/users/1')
+            .set('Authorization', "incorrect token")
+            .then(res => {
+                expect(res.body.message).toBe("Invalid Credentials");
+            })
+        })
+        it('should return json', function() {
+            //make a request to the api
+            return request(router).get('/api/users/1').then(res => {
+                expect(res.type).toMatch(/json/);
+            })
+        })
+    })
+    describe('delete /:id', function() {
+        it('not allow user to login without token', function() {
+            //make a request to the api
+            return request(router)
+            // .set('Content-Type', 'application/json')
+            .delete('/api/users/1')
+            .set('Authorization', "incorrect token")
+            .then(res => {
+                expect(res.body.message).toBe("Invalid Credentials");
+            })
+        })
+        it('should return json', function() {
+            //make a request to the api
+            return request(router).delete('/api/users/1').then(res => {
+                expect(res.type).toMatch(/json/);
+            })
+        })
+    })
 });
